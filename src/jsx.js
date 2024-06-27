33d684797447ef1549eaccaa6afd423cf0e664d5
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-06-27 22:03:23 +0300

    start project

---
+++ src/jsx.js
@@ -0,0 +1,50 @@
+export const Fragment = Symbol("Fragment");
+
+const jsx = (tag, attributes = {}, ...children) => {
+  attributes = attributes || {};
+
+  if (tag === Fragment) {
+    const fragment = document.createDocumentFragment();
+
+    children.forEach((child) => {
+      if (typeof child === "string" || typeof child === "number") {
+        fragment.append(document.createTextNode(child.toString()));
+      } else {
+        fragment.append(child);
+      }
+    });
+
+    return fragment;
+  }
+
+  if (typeof tag === "function") {
+    return tag(attributes, ...children);
+  }
+
+  const element = document.createElement(tag);
+
+  Object.entries(attributes).forEach(([key, value]) => {
+    if (key == "class") {
+      element.classList.add(...value.trim().split(" "));
+    } else if (key.startsWith("on") && key.toLowerCase() in window) {
+      element.addEventListener(key.toLowerCase().substring(2), value);
+    } else if (key === "style" && typeof value === "object") {
+      Object.assign(element.style, value);
+    } else {
+      element.setAttribute(key, value);
+    }
+  });
+
+  children.forEach((child) => {
+    if (typeof child === "string" || typeof child === "number") {
+      element.append(document.createTextNode(child.toString()));
+    } else if (Array.isArray(child)) {
+      child.forEach((innerChild) => element.append(innerChild));
+    } else {
+      element.append(child);
+    }
+  });
+  return element;
+};
+
+export default jsx;
